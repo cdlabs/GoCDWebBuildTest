58dc71182620052c3c678875d1d942dc0908c134
ifetayo <ifetayo.agunbiade@gamil.com>
2017-09-14 19:30:10 +0100

    init

---
+++ GoCDWebBuildTest/Controllers/GoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoCDWebBuildTest.Controllers
+{
+    public class GoController : Controller
+    {
+        // GET: Go
+        public string Index()
+        {
+            return "Go CD Build Test";
+        }
+    }
+}
